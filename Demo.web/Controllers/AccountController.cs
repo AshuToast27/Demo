31b2af7edc801ae550077ef5545887bef545c8b8
AshuToast27 <86969703+AshuToast27@users.noreply.github.com>
2023-10-19 10:58:59 +0530

    Add files via upload

---
+++ Demo.web/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Demo.web.Models.Domain;
+using Demo.web.Models.ViewModels;
+
+namespace Demo.web.Controllers
+{
+	
+	public class AccountController : Controller
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+
+		public AccountController(UserManager< ApplicationUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(AddEntryRequest model)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = new ApplicationUser { UserName = model.Mail, Email = model.Mail };
+				var result = await _userManager.CreateAsync(user, model.Password);
+
+				if (result.Succeeded)
+				{
+					// Generate email confirmation token
+					var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+					// Send email confirmation link to the user
+					var confirmationLink = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, token }, Request.Scheme);
+
+					// Send the confirmation link to the user's email address (implement your email sending logic here)
+					// Example: await _emailSender.SendEmailAsync(model.Email, "Confirm your email", $"Please confirm your email by clicking this link: {confirmationLink}");
+
+					// Redirect to a page indicating that email verification is required
+					return RedirectToAction("RegistrationConfirmation");
+				}
+
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError(string.Empty, error.Description);
+				}
+			}
+
+			return View(model);
+		}
+	}
+}
---
+++ Demo.web/Controllers/AddEntriesController.cs
@@ -0,0 +1,114 @@
+﻿using Azure;
+using Demo.web.Data;
+using Demo.web.Models.Domain;
+using Demo.web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Constraints;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Demo.web.Controllers
+{
+	public class AddEntriesController : Controller
+	{
+		private readonly DemoDbContext demoDbContext;
+		private IWebHostEnvironment hostenv;
+		public AddEntriesController(DemoDbContext demoDbContext, IWebHostEnvironment env)
+		{
+			this.demoDbContext = demoDbContext;
+			hostenv = env;
+		}
+
+
+		[HttpGet]
+		public IActionResult Show()
+		{
+			var entries = demoDbContext.Users.ToList();
+			return View(entries);
+		}
+
+
+
+
+
+		[HttpGet]
+		public IActionResult Add()
+		{
+			return View();
+		}
+
+
+		[HttpPost]
+		[ActionName("Add")]
+		public IActionResult Add(AddEntryRequest addEntry)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					string Filename = "";
+					if(addEntry.Photo != null) 
+					{
+						string uploadFolder = Path.Combine(hostenv.WebRootPath, "images");
+						Filename = Guid.NewGuid().ToString() + "_" + addEntry.Photo.FileName;
+						string Filepath= Path.Combine(uploadFolder, Filename);
+						addEntry.Photo.CopyTo(new FileStream(Filepath, FileMode.Create));
+
+					}
+
+					var entry = new User()
+					{
+						//mapping addTagRequest to tag domain model
+						FirstName = addEntry.Fname,
+						LastName = addEntry.Lname,
+						DateOfBirth = addEntry.Dob,
+						City = addEntry.City,
+						Address = addEntry.Address,
+						Mail = addEntry.Mail,
+						Photo = Filename
+					};
+					demoDbContext.Users.Add(entry);
+					demoDbContext.SaveChanges();
+					TempData["successMessage"] = "Entry Added!";
+					return RedirectToAction("Add");
+				}
+				else
+				{
+					TempData["errorMessage"] = "Entry Added!";
+					return RedirectToAction("Show");
+				}
+			}
+			catch (Exception e)
+			{
+
+				TempData["errorMessage"] = e.Message;
+				return RedirectToAction("Index");
+			}
+
+
+		}
+
+		[HttpPost]
+		public IActionResult DeleteSelected(int[] selectedIds)
+		{
+			if (selectedIds != null && selectedIds.Length > 0)
+			{
+				foreach (int id in selectedIds)
+				{
+					var user = demoDbContext.Users.Find(id);
+					if (user != null)
+					{
+						demoDbContext.Users.Remove(user);
+					}
+				}
+
+				demoDbContext.SaveChanges();
+			}
+
+			return RedirectToAction("Show");
+		}
+
+
+	}
+}
