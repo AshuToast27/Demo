3321c90c30b3ddf1e023e7db6341ab2a231d79b1
AshuToast27 <86969703+AshuToast27@users.noreply.github.com>
2023-10-19 11:02:41 +0530

    Add files via upload

---
+++ Demo.web/Program.cs
@@ -0,0 +1,42 @@
+using Demo.web.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DemoDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DemoDbConnectionString")));
+
+var app = builder.Build();
+
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<DemoDbContext>();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
+
